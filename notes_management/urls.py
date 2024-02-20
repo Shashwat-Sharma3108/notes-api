6e9efa37c42c03a5410e0e1711a314e20ef90225
shashwat-sharma <shashwat_sharma@trendmicro.com>
2024-02-20 13:02:44 +0530

    Removed unused code

--- notes_management/urls.py
+++ notes_management/urls.py
@@ -4,14 +4,8 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 from django.conf import settings
 from django.contrib import admin
 
-if settings.DEBUG:
-    router = DefaultRouter()
-else:
-    router = SimpleRouter()
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(r"api/", include("users.urls")),
     path(r"notes/", include("notes.urls")),
 ]
-urlpatterns += router.urls
